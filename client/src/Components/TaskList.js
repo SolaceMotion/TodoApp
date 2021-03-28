55db937dbb29429f8363cc5226fb5932e29daee9
SolaceMotion <SolaceMotion@users.noreply.github.com>
2021-03-28 16:57:25 +0200

    Fix files

--- client/src/Components/TaskList.js
+++ client/src/Components/TaskList.js
@@ -1,72 +1,181 @@
-import React from 'react'
-import axios from 'axios'
-import { Container, Button, TextField, Card, CardContent } from '@material-ui/core'
-import { green } from '@material-ui/core/colors'
-import { withStyles } from '@material-ui/core/styles'
+import React from "react"
+import axios from "axios"
+import {
+  Container,
+  Button,
+  TextField,
+  Card,
+  CardContent,
+  Box,
+} from "@material-ui/core"
+import { green } from "@material-ui/core/colors"
+import { withStyles } from "@material-ui/core/styles"
+import Swal from "sweetalert2"
 
 const ColorButton = withStyles(theme => ({
   root: {
-    backgroundColor: green[500],
-    '&:hover': {
-      backgroundColor: green[700],
+    color: green[500],
+    borderColor: green[500],
+    "&:hover": {
+      color: green[700],
+      borderColor: green[700],
     },
   },
-}))(Button);
-
+}))(Button)
 
 class TaskList extends React.Component {
-  state = {
-    task: '', // Entered task
-    taskList: [] // Store all tasks
+  constructor(props) {
+    super(props)
+    this.state = {
+      task: "", // Entered task
+      taskList: [], // Store all tasks
+      searchValue: "",
+      fields: [],
+    }
   }
 
   componentDidMount() {
+    //componentDidMount calls the getTaskList after all other components are loaded
     this.getTaskList()
   }
 
   getTaskList = () => {
-    axios.get('http://localhost:4000/tasks')
-    .then(response => response.data.recordset)
-    .then(response => {
-      this.setState({ taskList: response })
-    })
+    axios
+      .get("http://localhost:4000/tasks")
+      .then(response => response.data.recordset)
+      .then(response =>
+        this.setState({ taskList: response }, () => {
+          console.log(this.state.taskList) //See if a row is added and if state changes
+        })
+      )
+      .catch(err => err)
   }
 
   onSubmitClick = () => {
-    axios.post('http://localhost:4000/create-task', {
+    axios.post("http://localhost:4000/create-task", {
       task: this.state.task,
     })
     this.getTaskList()
-    this.setState({task: ''})
+    this.setState({ task: "" })
   }
-  onDeleteClick = (taskid) => {
+
+  onDeleteClick = taskid => {
     axios.delete(`http://localhost:4000/delete-task/${taskid}`)
     this.getTaskList()
   }
-  
+
+  onDeleteAll = () => {
+    axios.delete("http://localhost:4000/delete-all")
+    this.getTaskList()
+  }
+
   render() {
-    console.log(this.state.taskList)
+    /* SearchBar */
+    const filteredSearch = this.state.taskList.filter(task => {
+      //Filter search to return true only when search query matches a task
+      return task.tasks
+        .toLowerCase()
+        .startsWith(this.state.searchValue.toLowerCase()) //startsWith or include method toLowerCase so you can type in uppercase and lowercase
+    })
+
     return (
       <Container>
-        <h1>TaskList</h1>
-          {/*<input type="text" value={this.state.task} onChange={e => this.setState({ task: e.target.value })} placeholder="Task" /> */}
-          <TextField id="outlined-basic" label="Outlined" variant="outlined" value={this.state.task} onChange={e => this.setState({ task: e.target.value })}/>
-          <Button variant="contained" color="primary" onClick={() => this.onSubmitClick()}>Submit</Button>
-          <hr />
-            {this.state.taskList.map((task) => (
-              <Card>
+        <h1>TodoApp</h1>
+        <Box display="flex" justifyContent="space-between">
+          <div>
+            {/*<input type="text" value={this.state.task} onChange={e => this.setState({ task: e.target.value })} placeholder="Task" /> */}
+            <TextField
+              className="outlined-basic"
+              label="Type Todo"
+              variant="standard"
+              value={this.state.task}
+              onChange={e => this.setState({ task: e.target.value })}
+            />
+            <Button
+              size="large"
+              variant="outlined"
+              color="primary"
+              onClick={() => {
+                this.onSubmitClick()
+                Swal.fire("Todo Added!", "", "success")
+              }}
+            >
+              Add
+            </Button>
+
+            <Button
+              size="large"
+              variant="outlined"
+              color="primary"
+              onClick={() =>
+                Swal.fire({
+                  title: `Remove All Todos?`,
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonText: `Remove`,
+                }).then(result => {
+                  if (result.isConfirmed) {
+                    Swal.fire("Removed All Tasks!", "", "success")
+                    this.onDeleteAll()
+                  }
+                })
+              }
+            >
+              Remove All
+            </Button>
+          </div>
+          <TextField
+            className="outlined-basic"
+            label="Search Todos"
+            variant="standard"
+            value={this.state.searchValue}
+            onChange={e => this.setState({ searchValue: e.target.value })}
+          />
+        </Box>
+        <hr />
+        <Box display="flex" flexWrap="wrap" flexGrow={1} textAlign="center">
+          {filteredSearch.map((task, index) => {
+            return (
+              <Card
+                key={index}
+                style={{
+                  minWidth: 33.3 + "%",
+                  marginBottom: 5,
+                  marginTop: 5,
+                }}
+              >
                 <CardContent>
-                  <div>{task.tasks}</div>
+                  <h2>{task.tasks}</h2>
+                  <div>
                     <div>
-                      <div>
-                        <ColorButton variant="contained" color="secondary">Done</ColorButton>
-                        <Button variant="contained" color="secondary" onClick={() => this.onDeleteClick(task.taskid)}>Delete</Button>
-                      </div>
+                      <ColorButton
+                        fullWidth
+                        variant="outlined"
+                        onClick={() =>
+                          Swal.fire({
+                            title: `Remove This Todo?`,
+                            icon: "warning",
+                            showCancelButton: true,
+                            confirmButtonText: `Remove`,
+                          }).then(result => {
+                            if (result.isConfirmed) {
+                              Swal.fire("Removed!", "", "success")
+                              this.onDeleteClick(task.taskid)
+                            }
+                          })
+                        }
+                      >
+                        Done
+                      </ColorButton>
+                      {/* <Button variant="contained" color="secondary" onClick={() => this.onDeleteClick(task.taskid)}>Delete</Button> */}
                     </div>
-                  </CardContent>
-                </Card>
-              ))}
-            </Container>
+                  </div>
+                </CardContent>
+              </Card>
+            )
+          })}
+        </Box>
+      </Container>
     )
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,59 +1,57 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
+require("dotenv").config()
+const express = require("express")
+const cors = require("cors")
 
-const sql = require('mssql')
+const sql = require("mssql")
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
 
 const dbConfig = {
-	user: process.env.DB_USER,
-	password: process.env.DB_PASS,
-	server: process.env.DB_SERVER,
-	port: 3000,
-	database: process.env.DB_NAME,
-	stream: false,
-	options: {
-		port: 3000,
-		encrypt: false,
-		enableArithAbort: true
-	},
-	
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  server: process.env.DB_SERVER,
+  port: 3000,
+  database: process.env.DB_NAME,
+  stream: false,
+  options: {
+    port: 3000,
+    encrypt: false,
+    enableArithAbort: true,
+  },
 }
 
 sql.connect(dbConfig, err => {
   if (err) return console.error(err)
 
   const server = app.listen(4000, () => {
-    console.log('Listening on http://localhost:4000')
+    console.log("Listening on http://localhost:4000")
   })
-  server.on('close', sql.close.bind(sql))
+  server.on("close", sql.close.bind(sql))
 })
 
 /*-----Routes-----*/
 
-app.get('/tasks', (req, res) => {
+app.get("/tasks", (req, res) => {
   const request = new sql.Request()
 
   const TASK_QUERY = "SELECT * FROM TasksTbl"
 
   request.query(TASK_QUERY, (err, response) => {
     if (err) {
-        return console.log(err)
+      return console.log(err)
     }
     res.send(response)
   })
 })
 
-app.post('/create-task', (req, res) => {
+app.post("/create-task", (req, res) => {
   const request = new sql.Request()
-  request.input('tasks', sql.NVarChar, req.body.task) //Sql Parameter
+  request.input("tasks", sql.NVarChar, req.body.task) //Sql Parameter
 
   const ADD_QUERY = "INSERT INTO TasksTbl(tasks) VALUES(@tasks)"
-    
+
   request.query(ADD_QUERY, err => {
     if (err) {
       return console.log(err)
@@ -62,15 +60,25 @@ app.post('/create-task', (req, res) => {
   })
 })
 
-app.delete('/delete-task/:taskid', (req, res) => {
+app.delete("/delete-task/:taskid", (req, res) => {
   const request = new sql.Request()
-  request.input('taskid', sql.Int, req.params.taskid)
-  
+  request.input("taskid", sql.Int, req.params.taskid)
+
   const DELETE_QUERY = "DELETE FROM TasksTbl WHERE taskid = @taskid"
 
   request.query(DELETE_QUERY, (err, response) => {
     if (err) {
       return console.log(err)
+    }
+  })
+})
+
+app.delete("/delete-all", (req, res) => {
+  const request = new sql.Request()
+
+  request.query("TRUNCATE TABLE TasksTbl", err => {
+    if (err) {
+      return console.error(err)
     }
   })
 })
